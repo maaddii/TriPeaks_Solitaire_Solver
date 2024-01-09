11ecb5586d96f51e3ee3619f590867c121af2ee4
Maude Beaulieu-Laliberté <mabe0213@ulaval.ca>
2024-01-08 20:08:29 -0500

    Avancement dans les règles

--- deck.py
+++ deck.py
@@ -1,4 +1,4 @@
-""" Importations des exceptions """
+""" Exception imports """
 from exceptions import DeckError, CardNotValidError, DeckIncompleteError
 # 1H, 2H, 3H, 4H, 5H, 6H, 7H, 8H, 9H, 10H, 11H, 12H, 13H, 1D, 2D, 3D, 4D, 5D, 6D, 7D, 8D, 9D, 10D, 11D, 12D, 13D, 1C, 2C, 3C, 4C, 5C, 6C, 7C, 8C, 9C, 10C, 11C, 12C, 13C, 1S, 2S, 3S, 4S, 5S, 6S, 7S, 8S, 9S, 10S, 11S, 12S, 13S
 # 1h, 2h, 3h, 4h, 5h, 6h, 7h, 8h, 9h, 10h, 11h, 12h, 13h, 1d, 2d, 3d, 4d, 5d, 6d, 7d, 8d, 9d, 10d, 11d, 12d, 13d, 1c, 2c, 3c, 4c, 5c, 6c, 7c, 8c, 9c, 10c, 11c, 12c, 13c, 1s, 2s, 3s, 4s, 5s, 6s, 7s, 8s, 9s, 10s, 11s, 12s, 13s
@@ -6,43 +6,33 @@ from exceptions import DeckError, CardNotValidError, DeckIncompleteError
 
 class Deck:
     """"
-    blablabla
+    This class is used to verify if the deck given is valid.
     """
     def __init__(self, deck):
         self.deck = deck
 
     def cards_verif(self):
         """
-        blablabla
+        This method is used to verify if the deck given is valid.
         """
-        normal_deck = ['1H', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', '11H', '12H', '13H',
+        norm_d = ['1H', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', '11H', '12H', '13H',
                 '1D', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', '11D', '12D', '13D',
                 '1C', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', '11C', '12C', '13C',
                 '1S', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', '11S', '12S', '13S']
+
+        if self.deck == ['']:
+            raise DeckError("The deck is empty!")
+
         if len(self.deck) > 52:
-            raise DeckError("Le deck est trop grand.")
+            raise DeckError("The number of cards is too high!")
 
         for card in self.deck:
-            if card in normal_deck:
-                normal_deck.remove(card)
-                if not normal_deck:
-                    print("Le deck est complet!")
+            if card in norm_d:
+                norm_d.remove(card)
+                if not norm_d:
+                    print("The deck of card is valid!")
             else:
-                raise CardNotValidError(f"{card} n'est pas une carte valide.")
-        
-        if normal_deck:
-                raise DeckIncompleteError(f"Il manque la / les carte(s)"
-                                          f"suivante(s): {normal_deck}")
+                raise CardNotValidError(f"{card} is not valid.")
 
-# if "__main__" == __name__:
-#     my_deck = []
-#     cards = input("Entrez les cartes: ")
-#     cards = cards.upper()
-#     cards_list = cards.split(", ")
-
-#     for card in cards_list:
-#         my_deck.append(card)
-
-#     args = Deck(my_deck)
-#     args.card_verif()
-#     tri = TriPeaks()
+        if norm_d:
+            raise DeckIncompleteError(f"Missing the following card(s): {norm_d}")
--- tripeaks.py
+++ tripeaks.py
@@ -1,30 +1,96 @@
-""" blablabla """
+""" Deck and exceptions imports. """
 from deck import Deck
+from exceptions import DeckError, CardNotValidError, DeckIncompleteError
 
 
 class TriPeaks(Deck):
     """
-    blablabla
+    This class allows finding the best solution for the 'TriPeaks' game.
     """
-    def __init__(self):
-        super().__init__(self)
+    def __init__(self, deck):
+        super().__init__(deck)
 
     def tri_rangee(self):
         """
+        A method for separating the deck into 5 rows.
+        It allows visualizing the game.
+        """
+        rows = [self.deck[0:24], self.deck[24:34],
+                self.deck[34:43], self.deck[43:49], self.deck[49:52]]
+        res = []
+        for row in rows:
+            a = []
+            for card in row:
+                if len(card) == 3:
+                    a.append(card[0:2])
+                else:
+                    a.append(card[0])
+            res.append(a)
+        return res
+    
+    def tri_action(self):
+        """
         blablabla
         """
-        rangee1 = self.deck[0:10]
-        rangee2 = self.deck[11:19]
-        rangee3 = self.deck[20:25]
-        rangee4 = self.deck[26:28]
-        action_deck = self.deck[29:52]
-        return [rangee1, rangee2, rangee3, rangee4, action_deck]
+        r = self.tri_rangee()
+        r1 = r[1]
+        r2 = r[2]
+        r3 = r[3]
+        r4 = r[4]
+        action_card = int(r[0][0])
+        num1 = 0
+        num2 = 0
+
+        for n in range(0, 24):
+            action_card = int(action_card)
+            print(action_card)
+            if action_card+1 == 14:
+                num1 = 1
+            else:
+                num1 = action_card + 1
+
+            if action_card-1 == 0:
+                num2 = 13
+            else:
+                num2 = action_card - 1
+
+            print(num1, num2)
+
+            if str(num1) in r4:
+                action_card = num1
+                index = r4.index(str(action_card))
+                r4[index] = 'X'
+                print(action_card)
+            elif str(num2) in r4:
+                action_card = num2
+                index = r4.index(str(action_card))
+                r4[index] = 'X'
+                print(action_card)
+            else:
+                action_card = int(r[0][n])
+                print(action_card)
+
+            if r4 == ['X', 'X', 'X']:
+                print(r4)
+                break
+            print(r4)
+            print()
 
 
 
 if "__main__" == __name__:
     my_deck = []
-    cards = input("Entrez les cartes: ")
+    cards = input("Welcome to TriPeaks Solver! \n"
+                  "1. Use the following notation to input the cards: "
+                  "4D would be 4 of Diamonds, 12C would be Queen of Clubs, "
+                  "1H would be Ace of Hearts etc."
+                  "(1 to 13 for the value and H, D, C, S for the suit) \n"
+                  "2. First card to 24th represent the deck of cards.\n"
+                  "3. 24th to 34th card represent the first row of the game, "
+                  "34th to 43th the second row, 43th to 49th the third row "
+                  "and 49th to 52th the fourth row. \n"
+                  "Enter the cards: "
+                  )
     cards = cards.upper()
     cards_list = cards.split(", ")
 
@@ -32,8 +98,12 @@ if "__main__" == __name__:
         my_deck.append(card)
 
     args = Deck(my_deck)
+    t = TriPeaks(my_deck)
     args.cards_verif()
+    print(t.tri_rangee())
+    print()
+    t.tri_action()
 
-    tri = TriPeaks()
-    print(tri.tri_rangee())
 
+# My deck of cards:
+# 12D, 8C, 3D, 8D, 3H, 5D, 7S, 4D, 6C, 2D, 13S, 1C, 12S, 9H, 5C, 12C, 4C, 4H, 1D, 11C, 6D, 1S, 3C, 13D, 7H, 5S, 5H, 2S, 9C, 7D, 2H, 9S, 13H, 9D, 10H, 12H, 8S, 11S, 13C, 3S, 11D, 8H, 1H, 4S, 10C, 6H, 10S, 6S, 7C, 2C, 11H, 10D
